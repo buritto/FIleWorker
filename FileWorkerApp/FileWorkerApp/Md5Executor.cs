96ff4fe629d6e83ce2c77ebd377d626d0c392695
Arthur Karamow <ybiz177@gmail.com>
2018-02-12 22:00:25 +0600

    Added Md5Executor class

---
+++ FileWorkerApp/FileWorkerApp/Md5Executor.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace FileWorkerApp
+{
+    public class Md5Executor : IExecutable
+    {
+        public bool Execute(FileStream file)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
