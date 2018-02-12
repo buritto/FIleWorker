62dd38f3b23c469ca54365ea57b1e4bfb71d9cfa
Arthur Karamow <ybiz177@gmail.com>
2018-02-12 21:56:05 +0600

    add FileWorker class and IExecutable interface

---
+++ FileWorkerApp/FileWorkerApp/FileWorker.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace FileWorkerApp
+{
+    public class FileWorker
+    {
+        private IExecutable command;
+        public bool IsRecursive { get; set; }
+        public string Path { get; set; }
+
+        public FileWorker(IExecutable command)
+        {
+            this.command = command;
+        }
+
+        public bool Execute()
+        {
+            var stream = new FileStream(Path, FileMode.Open);
+            return command.Execute(stream);
+        }
+    }
+
+
+}
---
+++ FileWorkerApp/FileWorkerApp/IExecutable.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace FileWorkerApp
+{
+    public interface IExecutable
+    {
+        bool Execute(FileStream file);
+    }
+}
